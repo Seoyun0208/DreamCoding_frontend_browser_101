7caf6c63c8793c84fa1058d93d97e17e6f9e2130
Seoyun0208 <84954568+Seoyun0208@users.noreply.github.com>
2022-04-18 20:12:42 +0900

    [REFACTOR] shopping_list 코드 개선
    이벤트 위임을 활용하여 shopping_list 의 기존 코드 개선

--- shopping_list/main.js
+++ shopping_list/main.js
@@ -2,55 +2,40 @@ const items = document.querySelector("#items");
 const input = document.querySelector("#footer__input");
 const addBtn = document.querySelector(".footer__add");
 
+// input 에 작성한 내용을 가지고 리스트를 만들어주는 함수
+// 삭제 버튼을 눌렀을 때 삭제 버튼에 있는 id 와 동일한 id 를 가진 item 을 찾아 삭제할 수 있도록, 삭제 버튼과 item 버튼에 동일한 id 를 부여함
+// 현업에서는 UUID 등을 이용하여 고유한 키를 만드는 것이 좋음!
+
+let id = 0;
+
 function createItem(text) {
-	// item__row 태그 만들기
 	const itemRow = document.createElement("ul");
 	itemRow.setAttribute("class", "item__row");
 
-	// item 태그 만들기
-	const item = document.createElement("li");
-	item.setAttribute("class", "item");
+	itemRow.innerHTML = `
+	<li class="item" data-id=${id}>
+		<label class="item__name">
+			<input class="item__checkbox" type="checkbox"/>
+			<span class="item__text">${text}</span>
+		</label>
+		<button class="item__delete" data-id=${id}>
+			<i class="fas fa-eraser"></i>
+		</button>
+		<div class="item__line"></div>
+	</li>
+	`;
 
-	// item__name 태그 만들기
-	const itemName = document.createElement("label");
-	itemName.setAttribute("class", "item__name");
+	id++;
 
-	// item__checkbox 태그 만들기
-	const itemCheckbox = document.createElement("input");
-	itemCheckbox.setAttribute("class", "item__checkbox");
-	itemCheckbox.setAttribute("type", "checkbox");
-
-	// item__text 태그 만들기
-	const itemText = document.createElement("span");
-	itemText.setAttribute("class", "item__text");
-	itemText.innerText = text;
-
-	// item__delete 태그 만들기
-	const itemDelete = document.createElement("button");
-	itemDelete.setAttribute("class", "item__delete");
-	itemDelete.innerHTML = '<i class="fas fa-eraser"></i>';
-
-	// 삭제 버튼 클릭시 item 삭제하기
-	itemDelete.addEventListener("click", () => {
-		items.removeChild(itemRow);
-	});
-
-	// item__line 태그 만들기
-	const itemLine = document.createElement("div");
-	itemLine.setAttribute("class", "item__line");
-
-	// 모든 태그 연결하기
-	itemRow.appendChild(item);
-	itemRow.appendChild(itemLine);
-	item.appendChild(itemName);
-	item.appendChild(itemDelete);
-	itemName.appendChild(itemCheckbox);
-	itemName.appendChild(itemText);
 	return itemRow;
 }
 
+// 사용자가 input 에 입력한 값을 받아와서 새로운 item 을 만들어 items 안에 추가하는 함수
+// 세부 1. input 에 입력된 값이 없으면 focus 만 유지
+// 세부 2. items 에 스크롤이 생기는 경우, 새 item 추가 시 해당 item 으로 스크롤되도록 해줌
+// 세부 3. item 이 추가되고 나서는 input 의 내용을 지우고 focus 유지
+
 function onAdd() {
-	// 1. 사용자가 입력한 텍스트 받아온다.
 	const text = input.value;
 
 	if (text === "") {
@@ -58,16 +43,12 @@ function onAdd() {
 		return;
 	}
 
-	// 2. 새로운 item 을 만든다. (체크박스 + 텍스트 + 삭제 버튼 + 구분선)
 	const newItem = createItem(text);
 
-	// 3. items 안에 새로 만든 item 을 추가한다.
 	items.appendChild(newItem);
 
-	// 4. 새로 추가된 아이템으로 스크롤한다.
 	newItem.scrollIntoView({ block: "center" });
 
-	// 5. input 을 초기화한다.
 	input.value = "";
 	input.focus();
 }
@@ -86,4 +67,22 @@ input.addEventListener("keypress", (e) => {
 // input 을 클릭하면 "내용을 입력하세요." 문구 없애기
 input.addEventListener("click", () => {
 	input.setAttribute("placeholder", "");
+});
+
+// 특정 item 의 삭제 버튼을 누르면 해당 item 삭제하기
+items.addEventListener("click", (e) => {
+	let targetId = 0;
+
+	if (e.target.nodeName === "I") {
+		targetId = e.target.parentNode.dataset.id;
+	} else if (e.target.nodeName === "BUTTON") {
+		targetId = e.target.dataset.id;
+	}
+
+	if (targetId) {
+		const toBeDeleted = document.querySelector(
+			`.item[data-id="${targetId}"]`
+		);
+		toBeDeleted.remove();
+	}
 });
